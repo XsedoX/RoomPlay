7d8fa9f3f242cb464fa2f8eb8e8fa6f9b10cf006
XsedoX <sjulek6011@gmail.com>
2025-08-18 21:20:58 +0200

    added feature WIP

--- frontend/features/f01.feature
+++ frontend/features/f01.feature
@@ -1,6 +1,6 @@
-Feature: F01 Logging - Logging in to the application
+Feature: F01 Login Menu
 
-  Scenario: User logs in with valid credentials
+  Scenario: User sees the apps logo
     Given I am on the login page
     When I click Log In with Google
     Then I should be redirected to the main menu page
---
+++ frontend/features/step_definitions/App.spec.ts
@@ -0,0 +1,15 @@
+import {expect} from '@vitest';
+
+
+
+Given('I am on the login page', function () {
+  return;
+});
+When('I click Log In with Google', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+Then('I should be redirected to the main menu page', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
