1e77b61fc77cba60cae993c298fbda30471ec64c
XsedoX <sjulek6011@gmail.com>
2025-08-18 21:20:41 +0200

    added vuetify and set up

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,11 +1,13 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 
+import createVuetify from './vuetify.ts';
 import App from './App.vue';
 import router from './router';
-
 const app = createApp(App);
 
+
+app.use(createVuetify);
 app.use(createPinia());
 app.use(router);
 
