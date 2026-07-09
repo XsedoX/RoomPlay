b3ee74161bad6ad289e452365b37e953bac906f0
Juliusz Sadowski <sjulek6011@gmail.com>
2026-07-09 22:09:06 +0200

    cleanup done - service layer removed

--- frontend/src/errors/validation_error.ts
+++
@@ -1,8 +0,0 @@
-export default class ValidationError extends Error {
-  public readonly fieldErrors: Record<string, string>;
-  constructor(message: string, fieldErrors: Record<string, string>) {
-    super(message);
-    this.name = 'ValidationError';
-    this.fieldErrors = fieldErrors;
-  }
-}
